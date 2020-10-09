6fca59f4ccd42384fe47e8df767fcffd5dfd1051
Hutger H <hutger@gmail.com>
2020-10-09 12:51:54 +0200

    Updating hiera

--- postgresql/server.pp
+++ postgresql/server.pp
@@ -1,6 +1,8 @@
 # modules/profile/manifests/postgresql/server.pp
 
-class profile::postgresql::server {
+class profile::postgresql::server (
+  Optional[Hash]  $profile::postgresql::server::dbs = undef, 
+){
   include '::postgresql::server'
   include '::postgresql::server::contrib'
   include '::postgresql::globals'

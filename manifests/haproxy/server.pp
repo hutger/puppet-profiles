f7ef5d8e7d7040813358cc45ef8172feceaaffa4
Hutger H <hutger@gmail.com>
2020-12-04 12:45:04 +0100

    adding haproxy settings

--- manifests/haproxy/server.pp
+++ manifests/haproxy/server.pp
@@ -1,6 +1,13 @@
 # This role builds puppet master server
 
 class profiles::haproxy::server {
-  include 'profiles::base_linux'
   include 'haproxy'
+
+  $haproxy_listen_services   = hiera_hash('roles::haproxy::server::listen_services',   {})
+  $haproxy_frontends         = hiera_hash('roles::haproxy::server::frontends',         {})
+  $haproxy_backends          = hiera_hash('roles::haproxy::server::backends',          {})
+
+  create_resources('::haproxy::listen',   $haproxy_listen_services)
+  create_resources('::haproxy::frontend', $haproxy_frontends)
+  create_resources('::haproxy::backend',  $haproxy_backends)
 }

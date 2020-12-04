9bf9b9a878ee80e0e060a509d117ecfbdf07825c
Hutger H <hutger@gmail.com>
2020-12-04 13:28:27 +0100

    Updating config

--- manifests/haproxy/server.pp
+++ manifests/haproxy/server.pp
@@ -3,11 +3,11 @@
 class profiles::haproxy::server {
   include 'haproxy'
 
-  $haproxy_listen_services   = hiera_hash('roles::haproxy::server::listen_services',   {})
+  $haproxy_balancer_members   = hiera_hash('roles::haproxy::server::balancermembers',   {})
   $haproxy_frontends         = hiera_hash('roles::haproxy::server::frontends',         {})
   $haproxy_backends          = hiera_hash('roles::haproxy::server::backends',          {})
 
-  #create_resources('::haproxy::listen',   $haproxy_listen_services)
+  create_resources('::haproxy::balancermember', $haproxy_balancer_members)
   create_resources('::haproxy::frontend', $haproxy_frontends)
   create_resources('::haproxy::backend',  $haproxy_backends)
 }

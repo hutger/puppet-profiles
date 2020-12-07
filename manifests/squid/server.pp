7dd8fd1a9fefb7e965154e5e0b1f2b7da66eb0ed
Hutger H <hutger@gmail.com>
2020-12-07 10:27:53 +0100

    Adding http_access and acls

--- manifests/squid/server.pp
+++ manifests/squid/server.pp
@@ -3,11 +3,13 @@
 class profiles::squid::server {
   include 'squid'
 
-  $squid_basics     = hiera_hash('roles::squid::server::basics',   {})
-  $squid_cache_dir  = hiera_hash('roles::squid::server::cache_dir',         {})
-  #$squid_backends          = hiera_hash('roles::squid::server::backends',          {})
+  $squid_basics       = hiera_hash('roles::squid::server::basics',   {})
+  $squid_cache_dir    = hiera_hash('roles::squid::server::cache_dir',         {})
+  $squid_acls         = hiera_hash('roles::squid::server::acls',          {})
+  $squid_http_accesses  = hiera_hash('roles::squid::server::http_accesses',          {})
 
   create_resources('::squid', $squid_basics)
   create_resources('::squid::cache_dir', $squid_cache_dir)
-  #create_resources('::squid::backend',  $squid_backends)
+  create_resources('::squid::acl',  $squid_acls)
+  create_resources('::squid::http_access',  $squid_http_accesses)
 }

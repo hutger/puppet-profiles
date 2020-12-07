7a819f7e1a8eca5b4b1fe093ea638aaeb1fc056d
Hutger H <hutger@gmail.com>
2020-12-07 11:43:44 +0100

    class { 'apache':

--- manifests/squid/server.pp
+++ manifests/squid/server.pp
@@ -8,7 +8,7 @@ class profiles::squid::server {
   $squid_acls         = hiera_hash('roles::squid::server::acls',          {})
   $squid_http_accesses  = hiera_hash('roles::squid::server::http_accesses',          {})
 
-  class { '::squid':
+  class { 'squid':
     cache_mem => lookup ('roles::squid::server::basics::cache_mem')
   }
 

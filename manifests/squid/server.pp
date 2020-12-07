f2ee8bb69eaa76a7c64f38a7e62d24d3f4003072
Hutger H <hutger@gmail.com>
2020-12-07 08:11:39 +0100

    Adding caching to puppet

--- manifests/squid/server.pp
+++ manifests/squid/server.pp
@@ -3,11 +3,11 @@
 class profiles::squid::server {
   include 'squid'
 
-  $squid_basics   = hiera_hash('roles::squid::server::basics',   {})
-  #$squid_frontends         = hiera_hash('roles::squid::server::frontends',         {})
+  $squid_basics     = hiera_hash('roles::squid::server::basics',   {})
+  $squid_cache_dir  = hiera_hash('roles::squid::server::cache_dir',         {})
   #$squid_backends          = hiera_hash('roles::squid::server::backends',          {})
 
   create_resources('::squid', $squid_basics)
-  #create_resources('::squid::frontend', $squid_frontends)
+  create_resources('::squid::cache_dir', $squid_cache_dir)
   #create_resources('::squid::backend',  $squid_backends)
 }

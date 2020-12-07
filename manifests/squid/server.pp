96e552a6bb8031c6114cd63edc9ecd3b72e701d8
Hutger H <hutger@gmail.com>
2020-12-07 10:58:37 +0100

    roles::squid::server::basics

--- manifests/squid/server.pp
+++ manifests/squid/server.pp
@@ -3,12 +3,12 @@
 class profiles::squid::server {
   include 'squid'
 
-  $squid_basics       = hiera_hash('roles::squid::server::basics',   {})
-  $squid_cache_dir    = hiera_hash('roles::squid::server::cache_dir',         {})
-  $squid_acls         = hiera_hash('roles::squid::server::acls',          {})
+  $squid_cache_mem      = hiera_hash('roles::squid::server::cache_mem',   {})
+  $squid_cache_dir      = hiera_hash('roles::squid::server::cache_dir',         {})
+  $squid_acls           = hiera_hash('roles::squid::server::acls',          {})
   $squid_http_accesses  = hiera_hash('roles::squid::server::http_accesses',          {})
 
-  create_resources('::squid', $squid_basics)
+  create_resources('::squid', $squid_cache_mem)
   create_resources('::squid::cache_dir', $squid_cache_dir)
   create_resources('::squid::acl',  $squid_acls)
   create_resources('::squid::http_access',  $squid_http_accesses)

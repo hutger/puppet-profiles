38687547e29f7c0b3c910e654380e0f85f7e4174
Hutger H <hutger@gmail.com>
2021-01-25 15:44:41 +0100

    Adding netplan profile

--- manifests/netplan.pp
+++ manifests/netplan.pp
@@ -1,19 +1,16 @@
 # Base profile containg all the configuration
 class profiles::netplan (
+    $version = hiera('netplan::version', '2'),
+    $renderer = hiera('netplan::renderer', 'networkd'),
+    $config_file = hiera('config_file', '/etc/netplan/01-custom.yaml'),
+    $purge_config = hiera('purge_config', 'true'),
+    $netplan_apply = hiera('netplan_apply', 'true'),
+    $ethernets = hiera('netplan::ethernets', {}),
 ){
   class { 'netplan':
-    config_file   => '/etc/netplan/01-custom.yaml',
-    ethernets     => {
-      'ens5' => {
-        'dhcp4' => false
-      }
-    },
-    bridges       => {
-       'br0' => {
-          'dhcp4' => true,
-          'interfaces' => [ens5]
-       }
-    },
-    netplan_apply => true,
-  }
+    version     => $version,
+    renderer    => $renderer,
+    config_file => $config_file,
+    ethernets   => $ethernets,
+}
 }

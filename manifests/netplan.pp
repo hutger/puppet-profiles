05cc240dda2bb4eccb3301da7b38a61dde7b181f
Hutger H <hutger@gmail.com>
2021-01-25 15:19:30 +0100

    class { 'netplan':     config_file   => '/etc/netplan/01-custom.yaml',     ethernets     => {       'ens5' => {         'dhcp4' => false       }     },     bridges       => {        'br0' => {           'dhcp4' => true,           'interfaces' => [ens5]        }     },     netplan_apply => true,   }

--- manifests/netplan.pp
+++ manifests/netplan.pp
@@ -2,4 +2,19 @@
 class profiles::netplan (
 ){
   include 'netplan'
+
+    config_file   => '/etc/netplan/01-custom.yaml',
+    ethernets     => {
+      'ens5' => {
+        'dhcp4' => false
+      }
+    },
+    bridges       => {
+       'br0' => {
+          'dhcp4' => true,
+          'interfaces' => [ens5]
+       }
+    },
+    netplan_apply => true,
+  }
 }
